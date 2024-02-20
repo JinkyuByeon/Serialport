58ea755723567fd5a4d21cb8b4aef2746257d662
BYEON JINKYU <fullmetal@kakao.com>
2024-02-20 11:13:58 +0900

    ver 0.2 디자인 추가

--- day1/Form1.Designer.cs
+++ day1/Form1.Designer.cs
@@ -29,21 +29,146 @@ namespace day1
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox_port = new System.Windows.Forms.ComboBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button_disconnect = new System.Windows.Forms.Button();
+            this.textBox_send = new System.Windows.Forms.TextBox();
+            this.richTextBox_received = new System.Windows.Forms.RichTextBox();
+            this.label_send = new System.Windows.Forms.Label();
+            this.label_receive = new System.Windows.Forms.Label();
+            this.label_port = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.label_status = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // comboBox_port
+            // 
+            this.comboBox_port.FormattingEnabled = true;
+            this.comboBox_port.Location = new System.Drawing.Point(40, 47);
+            this.comboBox_port.Name = "comboBox_port";
+            this.comboBox_port.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_port.TabIndex = 0;
+            // 
+            // button_connect
+            // 
+            this.button_connect.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_connect.Location = new System.Drawing.Point(40, 210);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Padding = new System.Windows.Forms.Padding(200);
+            this.button_connect.Size = new System.Drawing.Size(81, 47);
+            this.button_connect.TabIndex = 1;
+            this.button_connect.Text = "연결하기";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.Button_connect_Click);
+            // 
+            // button_disconnect
+            // 
+            this.button_disconnect.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_disconnect.Location = new System.Drawing.Point(160, 210);
+            this.button_disconnect.Name = "button_disconnect";
+            this.button_disconnect.Size = new System.Drawing.Size(81, 47);
+            this.button_disconnect.TabIndex = 2;
+            this.button_disconnect.Text = "연결끊기";
+            this.button_disconnect.UseVisualStyleBackColor = true;
+            // 
+            // textBox_send
+            // 
+            this.textBox_send.Location = new System.Drawing.Point(338, 364);
+            this.textBox_send.Name = "textBox_send";
+            this.textBox_send.Size = new System.Drawing.Size(316, 21);
+            this.textBox_send.TabIndex = 3;
+            // 
+            // richTextBox_received
+            // 
+            this.richTextBox_received.Location = new System.Drawing.Point(338, 59);
+            this.richTextBox_received.Name = "richTextBox_received";
+            this.richTextBox_received.Size = new System.Drawing.Size(413, 249);
+            this.richTextBox_received.TabIndex = 4;
+            this.richTextBox_received.Text = "";
+            // 
+            // label_send
+            // 
+            this.label_send.AutoSize = true;
+            this.label_send.Location = new System.Drawing.Point(336, 343);
+            this.label_send.Name = "label_send";
+            this.label_send.Size = new System.Drawing.Size(29, 12);
+            this.label_send.TabIndex = 5;
+            this.label_send.Text = "송신";
+            // 
+            // label_receive
+            // 
+            this.label_receive.AutoSize = true;
+            this.label_receive.Location = new System.Drawing.Point(336, 35);
+            this.label_receive.Name = "label_receive";
+            this.label_receive.Size = new System.Drawing.Size(29, 12);
+            this.label_receive.TabIndex = 6;
+            this.label_receive.Text = "수신";
+            // 
+            // label_port
+            // 
+            this.label_port.AutoSize = true;
+            this.label_port.Location = new System.Drawing.Point(38, 32);
+            this.label_port.Name = "label_port";
+            this.label_port.Size = new System.Drawing.Size(90, 12);
+            this.label_port.TabIndex = 7;
+            this.label_port.Text = "COM 포트 설정";
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(676, 364);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(75, 23);
+            this.button_send.TabIndex = 8;
+            this.button_send.Text = "보내기";
+            this.button_send.UseVisualStyleBackColor = true;
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(12, 296);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(53, 12);
+            this.label_status.TabIndex = 9;
+            this.label_status.Text = "연결상태";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.label_port);
+            this.Controls.Add(this.label_receive);
+            this.Controls.Add(this.label_send);
+            this.Controls.Add(this.richTextBox_received);
+            this.Controls.Add(this.textBox_send);
+            this.Controls.Add(this.button_disconnect);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.comboBox_port);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_port;
+        private System.Windows.Forms.Button button_connect;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button_disconnect;
+        private System.Windows.Forms.TextBox textBox_send;
+        private System.Windows.Forms.RichTextBox richTextBox_received;
+        private System.Windows.Forms.Label label_send;
+        private System.Windows.Forms.Label label_receive;
+        private System.Windows.Forms.Label label_port;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.Label label_status;
     }
 }
 
--- day1/Form1.cs
+++ day1/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO.Ports;
+using System.IO.Ports; // 포트 필수 코드
 
 namespace day1
 {
@@ -18,9 +18,42 @@ namespace day1
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e) 
         {
-
+            comboBox_port.DataSource = SerialPort.GetPortNames(); // 연결 가능한 시리얼 포트 이름을 콤보 박스에 가져온다.
         }
+
+        private void Button_connect_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen) // 시리얼 포트가 열려 있지 않다면
+            {
+                serialPort1.PortName = comboBox_port.Text; // 콤보 박스의 선택된 COM포트명을 시리얼포트명으로 지정
+                serialPort1.BaudRate = 9600;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Parity = Parity.None;
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived); // 필수 코드
+
+                serialPort1.Open(); // 시리얼 포트 열기
+
+                label_status.Text = "포트가 열렸습니다.";
+                comboBox_port.ENabled = false; // COM포트 설정 콤보박스 비활성화
+            }
+            else // 시리얼 포트가 열려 있다면
+            {
+                label_status.Text = "포트가 이미 열려 있습니다.";
+            }
+        }
+        private void serialPort1_DataReceived(object sender, SerialErrorReceivedEventArgs e) // 수신 이벤트가 발생하면 이 부분이 실행
+        {
+            this.Invoke(new EventHandler(MySerialReceived)); // 메인 쓰레드와 수신 쓰레드의 충돌 방지를 위해 invoke 사용. MySerialReceived로 이동 추가 실행.
+        }
+        private void MySerialReceived(object s, EventArgs e) // 수신데이타 용도에 따라 처리
+        {
+            int ReceiveData = serialPort1.Rea
+        }
+
     }
+    
+    
 }
